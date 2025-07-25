826859ea4a68be5486b2c7862277f282ab862610
fiddystatic <75876698+fiddystatic@users.noreply.github.com>
2025-07-25 15:00:24 +0200

    Patched navbar icons & changed cart notification character.

--- map.js
+++ map.js
@@ -2,7 +2,8 @@ import * as L from 'leaflet';
 
 let googleMapsApiLoaded = false;
 let googleMapsApiLoading = false;
-const API_KEY = "AIzaSyBM8F2gXXiOXJQbW7JXPPtBI5KqPplrl3E"; // As provided by user
+//As provided below (Add your personal API key from Google Cloud Console: add - Geocoding API, Maps JavaScript API, and Places API)
+const API_KEY = "AIzaSyBM8F2gXXiOXJQbW7JXPPtBI5KqPplrl3E"; // Replace with your actual API key or else map features will not work.
 
 function loadGoogleMapsApi() {
     return new Promise((resolve, reject) => {
